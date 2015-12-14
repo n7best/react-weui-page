5fabea6812ba2929ee1c2d6027e6d7f7df69cb91
n7best <n7best@gmail.com>
2015-12-14 11:41:45 -0500

    add support sponsor

--- src/components/page-main.jsx
+++ src/components/page-main.jsx
@@ -5,6 +5,9 @@ const Main = () => (
     <img src='logo.png' />
     <h1>n7-React-WeUI Documentation</h1>
     <h4>n7-react-weui 是官方的第三方分支</h4>
+    <hr/>
+    <h4>Support & Sponsor</h4>
+    <a href="https://www.browserstack.com/"><img src="https://camo.githubusercontent.com/f531fe477ddd1655dca7f50362659c38a6d6a803/68747470733a2f2f6a71756572792e6f72672f7265736f75726365732f6d656d626572732f62726f77736572737461636b2e706e67"/></a>
   </div>
 );
 
